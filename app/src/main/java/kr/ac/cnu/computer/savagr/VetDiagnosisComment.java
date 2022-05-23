01c889ade53521b004d149094f29332f0ec21230
danhyunjo <jdh33114@naver.com>
2022-05-23 20:28:21 +0900

    변경

---
+++ app/src/main/java/kr/ac/cnu/computer/savagr/VetDiagnosisComment.java
@@ -0,0 +1,4 @@
+package kr.ac.cnu.computer.savagr;
+
+public class VetDiagnosisComment {
+}
---
+++ app/src/main/java/kr/ac/cnu/computer/savagr/VetFragmentDiagnosisComment.java
@@ -0,0 +1,19 @@
+package kr.ac.cnu.computer.savagr;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class VetFragmentDiagnosisComment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_vet_comment, container, false);
+    }
+}
+
