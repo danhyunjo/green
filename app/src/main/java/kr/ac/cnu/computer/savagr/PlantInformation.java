9ce6e75b9731a8d61a51063fac21d61d350f0dc0
danhyunjo <jdh33114@naver.com>
2022-05-30 19:30:46 +0900

    변경

---
+++ app/src/main/java/kr/ac/cnu/computer/savagr/PlantInformation.java
@@ -0,0 +1,25 @@
+package kr.ac.cnu.computer.savagr;
+
+public class PlantInformation {
+    public String plants_name;
+    public String growth;
+    public String pest_control;
+    //public String seasonResult;
+    //public String lightResult;
+    public String soil_waterResult;
+    public String locationResult;
+
+    public PlantInformation() {
+
+    }
+
+    public PlantInformation(String plants_name, String growth, String pest_control, String soil_waterResult, String locationResult) {
+        this.plants_name = plants_name;
+        this.growth = growth;
+        this.pest_control = pest_control;
+        //this.seasonResult = seasonResult;
+        this.locationResult = locationResult;
+        this.soil_waterResult = soil_waterResult;
+        //this.lightResult = lightReult;
+    }
+}
